ee85d626397ea59104b3de581021bf873f03e1da
mconkla <46657361+mconkla@users.noreply.github.com>
2020-10-09 12:49:57 +0200

    basic player movement + test floor/player
    added basic player/camera movement script , added test sprites, added test floor

---
+++ Assets/Scripts/followPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+
+    Transform playerTransform;
+
+    [Range(8,20)]
+    public float offSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = FindObjectOfType<playerMovement>().myTF;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setPos();
+    }
+
+    void setPos()
+    {
+        this.transform.position = new Vector3(playerTransform.position.x + offSet, this.transform.position.y, this.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    [HideInInspector]
+    public Transform myTF;
+
+    Rigidbody2D myRB;
+
+    [Range(0, 5)]
+    public float movementSpeed = 0.5f;
+
+    [Range(8, 20)]
+    public float jumpForce = 2f;
+    float jF;
+
+    Vector3 movementVector;
+    Vector2 jumpVector;
+
+    bool jumpAllowed = true;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        jF = jumpForce * 100f;
+        myTF = GetComponent<Transform>();
+        myRB = GetComponent<Rigidbody2D>();
+        movementVector = new Vector3(movementSpeed, 0,0);
+        jumpVector = new Vector2(0, jF);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+        getInput();
+    }
+
+    void getInput()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump();
+        }
+
+    }
+
+    void move()
+    {
+        myTF.position = myTF.position + movementVector;
+    }
+    void jump()
+    {
+
+        if (jumpAllowed)
+        {
+            Debug.Log("JUMP");
+            myRB.AddForce(jumpVector);
+            jumpAllowed = false;
+        }
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.tag == "floor")
+        {
+            jumpAllowed = true;
+        }
+    }
+
+  
+}
